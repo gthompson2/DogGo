70885ce517600ceccd78fee99ad0e71b968e6f9e
Gregory Thompson <mrgregorythompsonii@gmail.com>
2021-03-29 09:46:05 -0500

    created Owner class and implemented Index and Detail

--- DogGo/Models/Owner.cs
+++ DogGo/Models/Owner.cs
@@ -5,12 +5,16 @@ using System.Threading.Tasks;
 
 namespace DogGo.Models
 {
-    public class Walker
+    public class Owner
     {
         public int Id { get; set; }
+        public string Email { get; set; }
         public string Name { get; set; }
+        public string Address { get; set; }
         public int NeighborhoodId { get; set; }
-        public string ImageUrl { get; set; }
+        public string Phone { get; set; }
         public Neighborhood Neighborhood { get; set; } // pulls whole neighborhood object to display the name
+                                                        // The conroller needs access to the Neighborhood object (I think... C# go brrrrrrr!)
+
     }
 }
